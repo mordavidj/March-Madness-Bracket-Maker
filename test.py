39b4986b0aa2e31dbb77f3f8855f7456653d8944
Dave Moreno <mordavidj@gmail.com>
2023-04-08 01:13:32 -0600

    Use season variable instead of hardcode

--- test.py
+++ test.py
@@ -4,17 +4,19 @@ from math import log
 
 INITIAL_ELO_RATING = 1300
 K = 20
+SEASON = 2023
 
 teams_df = pd.read_csv("data_2023/MTeams.csv", index_col='TeamID')
-teams_df = teams_df[teams_df['LastD1Season'] == 2023]
+teams_df = teams_df[teams_df['LastD1Season'] == SEASON]
 
-elo_df = teams_df.assign(eloRating=INITIAL_ELO_RATING)[['eloRating']]
-elo_df['eloRating'] = INITIAL_ELO_RATING
+elo_df = teams_df
+elo_df[SEASON] = INITIAL_ELO_RATING
+elo_df = elo_df[[SEASON]]
 
 print(elo_df)
 
 reg_season_df = pd.read_csv("data_2023/MRegularSeasonCompactResults.csv.zip", compression='zip')
-reg_season_df = reg_season_df[reg_season_df['Season'] == 2023]
+reg_season_df = reg_season_df[reg_season_df['Season'] == SEASON]
 print(reg_season_df)
 
 # https://fivethirtyeight.com/features/how-we-calculate-nba-elo-ratings/
@@ -23,10 +25,10 @@ for index, row in reg_season_df.iterrows():
     # w_ind = elo_df.index[elo_df['TeamID'] == row['WTeamID']]
     # print(row['WTeamID'])
     # print(type(row['WTeamID']))
-    w_elo = elo_df.at[row['WTeamID'], 'eloRating']
+    w_elo = elo_df.at[row['WTeamID'], SEASON]
     # print(f'w_elo: {w_elo}')
     # l_ind = elo_df.index[elo_df['TeamID'] == row['LTeamID']]
-    l_elo = elo_df.at[row['LTeamID'], 'eloRating']
+    l_elo = elo_df.at[row['LTeamID'], SEASON]
     # print(f'l_elo: {l_elo}')
 
     # ratings difference
@@ -41,8 +43,8 @@ for index, row in reg_season_df.iterrows():
     d_elo = (w_elo + K * (v_mar - we)) - w_elo
     # print(f'd_elo: {d_elo}')
 
-    elo_df.at[row['WTeamID'], 'eloRating'] = w_elo + d_elo
-    elo_df.at[row['LTeamID'], 'eloRating'] = l_elo - d_elo
+    elo_df.at[row['WTeamID'], SEASON] = w_elo + d_elo
+    elo_df.at[row['LTeamID'], SEASON] = l_elo - d_elo
 
     reg_season_df.at[index, 'eloChange'] = d_elo
 
@@ -53,3 +55,5 @@ print()
 print(elo_df.max())
 print(elo_df.min())
 print(elo_df.mean())
+
+elo_df.to_csv('data_2023/eloRatings.csv')
